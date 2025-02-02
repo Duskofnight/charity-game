f82de84bb4791e219a0125311c235d33484f6a65
Jimmy <tranvanhongkhanh@gmail.com>
2025-02-02 14:06:46 -0700

    send login info

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
 
+import axios from "axios";
+
 function App() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [donation, setDonation] = useState();
+    const [donation, setDonation] = useState(0);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         alert(`Thanks ${username} for donating $${donation}!`);
+
+        const user = {
+            username,
+            password,
+            donation,
+        };
+
+        try {
+            const response = await axios.post(
+                "http://localhost:3001/donate",
+                user
+            );
+
+            if (response.status === 200) {
+                window.location.href = "/game-page";
+            }
+            setDonation("");
+            setUsername("");
+            setDonation("");
+        } catch (error) {
+            console.error("Donation failed", error);
+        }
     };
     return (
         <div id="app">
             <br></br>
             <form onSubmit={handleSubmit}>
-                <img src="../public/assets/logo.png" alt="logo" />
+                <img src="../assets/logo.png" alt="logo" />
                 <input
                     type="text"
                     name="username"
